b4a3935586943c771af4d94a444f2630ecdb642f
HMHMHMJUN <emmitt9619@gmail.com>
2025-05-17 17:31:19 +0900

    Clean root-level initial commit

---
+++ src/main/java/fastCampus/project_board/BoardStudyApplication.java
@@ -0,0 +1,13 @@
+package fastCampus.project_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardStudyApplication.class, args);
+	}
+
+}
